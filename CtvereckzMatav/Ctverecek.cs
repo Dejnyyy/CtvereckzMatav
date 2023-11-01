c2fb3194aa1e20796831926d681519781761ac4f
Dejnyyy <danslaby@email.cz>
2023-11-01 12:50:27 +0100

    Add project files.

---
+++ CtvereckzMatav/Ctverecek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CtvereckzMatav
+{
+    public partial class Ctverecek : UserControl
+    {
+        public Ctverecek()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CtvereckzMatav/Form1.cs
@@ -0,0 +1,20 @@
+namespace CtvereckzMatav
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
